e9bf26dc183511d4ede91253e879e61854419130
Suraj-Umarani <01fe22bca170@kletech.ac.in>
2024-02-07 15:13:07 +0530

    Code for Calculator with multi functions

---
+++ Opertion.java
@@ -0,0 +1,16 @@
+public class Opertion {
+    // Returns the square of a number
+    public float square(float opr1) {
+        return (opr1 * opr1);
+    }
+
+    // Returns the cube of a number
+    public float cube(float opr1) {
+        return (opr1 * opr1 * opr1);
+    }
+
+    // Returns the modulus of two numbers
+    public float modulus(float opr1, float opr2) {
+        return (opr1 % opr2);
+    }
+}
